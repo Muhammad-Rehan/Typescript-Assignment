139c44d644f5b73ad6d99bd8cb7aac458dd14a44
Muhammad <muhammadrehan320@gmail.com>
2024-03-08 21:20:40 +0500

    Assignment 5 done

---
+++ Assignment#5/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// q-5 Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message.
+const famous_person = 'Albert Einstein';
+const message = 'Life is like riding a bicycle.To keep our balance,you must keep moving';
+console.log(`${famous_person}, once said, ${message}`);
