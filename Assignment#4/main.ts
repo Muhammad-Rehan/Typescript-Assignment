ebea2705e7b93cea112f2b3072f2e34aa19f7a3b
Muhammad <muhammadrehan320@gmail.com>
2024-03-08 21:14:23 +0500

    Assignment 4 done

---
+++ Assignment#4/main.ts
@@ -0,0 +1,7 @@
+//Q-4 Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+// Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+const quote = "The most terrifying words in the English language are, I'm from the government and I'm here to help."
+
+console.log(`Dwayne Johnson once said, "${quote}"`)
