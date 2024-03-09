6827a27d03f103faa6e15192f45802f1f6448ae7
Muhammad <muhammadrehan320@gmail.com>
2024-03-09 21:33:28 +0500

    Assignment 10 done

---
+++ Assignment#10/main.js
@@ -0,0 +1,10 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+// If you don’t have anything specific to write because your programs are too simple at this point, 
+// just add your name and the current date at the top of each program file. Then write one sentence describing 
+// what the program does.
+// Declaring a Variable 
+var firstName;
+// Assigning name to variable
+firstName = 'Muhammad Rehan';
+// Printing my name in the console
+console.log(firstName);
