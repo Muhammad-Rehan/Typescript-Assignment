5e72ba002a93e8771ad09d9b7f74c4d38ba554b6
Muhammad <muhammadrehan320@gmail.com>
2024-03-10 21:57:58 +0500

    Assignment 11,12 & 13 done

---
+++ Assignment#11 & 12/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Q-11 -> Names: Store the names of a few of your friends in a array called names. 
+// Print each person’s name by accessing each element in the list, one at a time.
+const names = ['Waseem', 'Nofil', 'Saad'];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+//Q-12 -> Greetings: Start with the array you used in Exercise 11, 
+// but instead of just printing each person’s name, print a message to them. 
+// The text of each message should be the same, but each message should be personalized with the person’s name.
+console.log(`${names[0]}, Welcome to the AI course`);
+console.log(`${names[1]}, Welcome to the AI course`);
+console.log(`${names[2]}, Welcome to the AI course`);
---
+++ Assignment#13/README.md
@@ -0,0 +1,15 @@
+## Assignment Q-13
+- Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, 
+and make a list that stores several examples. Use your list to print a series of statements 
+about these items, such as “I would like to own a Honda motorcycle.”
+
+## Solution code
+
+```typescript
+const favCars = ['Lamborgani', 'Bently', 'Ferrai', 'Mazda Rx-8']
+
+console.log(`I would like to own a ${favCars[0]} car`)
+console.log(`I would like to own a ${favCars[1]} car`)
+console.log(`I would like to own a ${favCars[2]} car`)
+console.log(`I would like to own a ${favCars[3]} car`)
+```
